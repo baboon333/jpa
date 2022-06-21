93183555b0213387271da0d35bcf2cde4ac0f220
supercode333 <gacomonkey123@gmail.com>
2022-06-21 22:51:07 +0900

    도메인 생성

--- study/ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ study/ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
 
             tx.commit();
         } catch (Exception e) {
